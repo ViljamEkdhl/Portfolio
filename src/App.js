3274e70d597b662837135b6c004071d2c89d6dea
William Ekdahl <83842951+ViljamEkdhl@users.noreply.github.com>
2022-07-21 11:31:49 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import { Button, TextField } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-   <TextField id="standard-basic" label="Standard" variant="standard" />
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+
+				<div>
+					<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+				</div>
+
+				<div>
+					<Button variant="contained">Contained</Button>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
