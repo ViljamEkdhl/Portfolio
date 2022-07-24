31743907b2bf980504a2f5e2fcd57bb15ee57664
William Ekdahl <83842951+ViljamEkdhl@users.noreply.github.com>
2022-07-24 20:38:06 +0200

    Fancy slide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
 import TopBar from "./components/TopBar";
-
-import {Parallax, ParallaxLayer } from 
+import background from "./images/background.jpg";
+import {Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 function App() {
 	return (
+
 		<div className="App">
-		<TopBar/>
+
+		<Parallax pages={2}>
+		<ParallaxLayer 
+			offset={0}
+			speed={2.5}
+			factor={1}
+			style=
+			{{
+				backgroundColor: "white",
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				color: 'white',
+			}}>
+
+			<h1 className="main-title">SBOTIFY</h1>
+		</ParallaxLayer>
+
+		<ParallaxLayer offset={0} speed={2} sticky={{start: 0, end: 1}}>
+			<TopBar/>
+		</ParallaxLayer>
+
+		<ParallaxLayer offset={1} speed={2} style={{ backgroundColor: "#22A377"}} />
+
+		<ParallaxLayer
+    	offset={1}
+    	speed={2}
+    	style={{
+      		display: 'flex',
+      		justifyContent: 'center',
+      		alignItems: 'center',
+      		color: 'white',
+			backgroundColor: '#22A377',
+    	}}>
+    		CONTENT
+  		</ParallaxLayer>
+		</Parallax>
+
 		</div>
 	);
 }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import background from "../images/background.jpg";
 import "../css/index.css";
 
 function TopBar() {
     return ( 
         <header className='header'>
+          <Button variant="outlined" >
+            Delete
+          </Button>
+          <Button variant="contained" >
+            Send
+          </Button>
 				<h1>SBOTIFY</h1>
         </header>
     );
