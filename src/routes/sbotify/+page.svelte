a5148e658b565916490eccf2022f00f851f69d3f
William Ekdahl <83842951+ViljamEkdhl@users.noreply.github.com>
2023-02-22 09:44:13 +0100

    example style

--- src/routes/sbotify/+page.svelte
+++ src/routes/sbotify/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import sbotifyProfile from "../../assets/sbotifyProfile.jpg"
-    import examplePrint from "../../assets/examplePrint.jpg"
+    import sbotifyProfile from "../../assets/sbotifyProfile.svg"
+    import examplePrint from "../../assets/examplePrint.svg"
 </script>
 
 <div>
@@ -16,8 +16,18 @@
 
 <section>
     <img src={sbotifyProfile} alt="sbotify profile">
+</section>
+
+<section>
     <img src={examplePrint} alt="Example sbotify print">
 </section>
+
 <style>
-    
+    section{
+        background-color: black;
+        margin-bottom: 20px;
+        border-style: none;
+        border-radius: 0px 40px 40px 0px;
+        height: 25rem;
+    }
 </style>
