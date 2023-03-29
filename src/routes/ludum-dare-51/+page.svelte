36584299df11137d44e327a5f3d82082ef8bc66f
William Ekdahl <viljam89@hotmail.com>
2023-03-29 22:24:38 +0200

    ludum-dare-51 iframe

--- src/routes/ludum-dare-51/+page.svelte
+++ src/routes/ludum-dare-51/+page.svelte
@@ -2,10 +2,29 @@
 
 </script>
 
-<div>
- <h1>Mer info</h1>
+<div class="page">
+    <iframe class="woe" id="woe"
+    title="WoE Password Page"
+    width="50%"
+    height="90%"
+
+    src="https://ludum-dare-51.vercel.app/"
+    >
+    </iframe>
 </div>
 
 <style>
-    
+    .woe{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin: auto;
+        border-radius: 30px;
+        border-style:hidden;
+    }
+
+    .page{
+        margin-top: 4rem;
+        height: 85vh;
+    }
 </style>
